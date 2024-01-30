365db3fd66618db09d687778cf0133a38b71d38b
chaim96 <chaim@campus.technion.ac.il>
2024-01-30 19:46:37 +0200

    Completed my functions

--- RRT_Star.py
+++ RRT_Star.py
@@ -9,8 +9,9 @@ class Vertex:
         self.cost = cost
         self.parent_index = parent_index
 
+
 class Graph:
-    def __init__(self, start, end, obstacles):
+    def __init__(self, start, end, obstacles, v_change_range, delta_change_range, max_delta_per_v, t_max):
         # graph attributes
         self.start = start
         self.end = end
@@ -18,65 +19,121 @@ class Graph:
         self.vertexes = [start]
         self.path = []
         self.end_radius = 15
+        self.v_change_range = v_change_range # a new v will be within [v_current - v_change_range, v_current + v_change_range]
+        self.delta_change_range = delta_change_range # a new delta will be within [delta_current - delta_change_range, delta_current + delta_change_range]
+        self.max_delta_per_v = max_delta_per_v # an array which contains the max delta possible for a given v
+        self.t_max = t_max # max time to new vertex
+        self.iteration = 0 # stores number of iterations
 
-    # adding & removing vertexes and edges
+
+    # adds new vertex
     def add_vertex(self, vertex):
         self.vertexes.append(vertex)
 
-    #choose random vertex to be the parent- scan the vertexes list
-    #retun the object of the chosen vertex
+
+    # chooses random vertex to be the parent
+    # returns the object of the chosen vertex
     def choose_random_existing_vertex(self):
-        #TODO: Chaim
+        num_of_vertexes = len(self.vertexes)
+        random_vertex_index = random(0, num_of_vertexes-1)
+        return self.vertexes[random_vertex_index]
 
-    #choose random delta, v, t
+
+    # chooses random delta, v, t and return them
     def choose_random_parameters(self, vertex):
-        # TODO: Chaim
-        #first choose random v and by that choose random delta
+        valid_delta_for_v = False
+        while not valid_delta_for_v:
+            v = random(vertex.v - self.v_change_range, vertex.v + self.v_change_range)
+            # check if the lower limit of the random delta range is smaller\equal to the max delta allowed by v
+            # if not, choose a new v
+            if (vertex.delta - self.delta_change_range) <= self.max_delta_per_v[v]:
+                valid_delta_for_v = True
+                # check if the higher limit of the random delta range is smaller\equal to the max delta allowed by v
+                if(vertex.delta + self.delta_change_range) <= self.max_delta_per_v[v]:
+                    delta = random(vertex.delta - delta_change_range, vertex.delta + delta_change_range)
+                # if not, make the max delta the higher limit of the range
+                else:
+                    delta = random(vertex.delta - delta_change_range, self.max_delta_per_v[v])
+        t = random(0, self.t_max)
+        return delta, v, t
 
-    #returns number of vertexes in the graph
+
+    # returns number of vertexes in the graph
     def get_num_of_vertexes(self):
         return len(self.vertexes)
 
-    # calculates if new vertex in end radius
-    def is_in_end_radius(self, a, b): #TODO:chaim
-        (x1, y1) = (a[0], a[1])
-        (x2, y2) = (b[0], b[1])
-        px = (float(x1) - float(x2)) ** 2
-        py = (float(y1) - float(y2)) ** 2
+
+    # calculates if new vertex is in end radius
+    # returns True is yes
+    def is_in_end_radius(self, vertex):
+        px = (float(vertex.x) - float(self.end.x)) ** 2
+        py = (float(vertex.y) - float(self.end.y)) ** 2
+        distance = (px + py) ** (0.5)
+        if distance <= self.end_radius:
+            return True
+        return False
+
+
+    # returns distance of vertex to end vertex
+    def calc_distance_to_end(self, vertex):
+        px = (float(vertex.x) - float(self.end.x)) ** 2
+        py = (float(vertex.y) - float(self.end.y)) ** 2
         distance = (px + py) ** (0.5)
         return distance
 
+
+    # finds vertex nearest to end vertex and returns it (for bias expanding)
+    def find_nearest_vertex_to_end(self):
+        min_distance = self.calc_distance(self.vertexes[0])
+        nearest_vertex_to_end = self.vertexes[0]
+        for vertex in self.vertexes:
+            distance = self.calc_distance(vertex)
+            if distance < min_distance:
+                min_distance = distance
+                nearest_vertex_to_end = vertex
+        return nearest_vertex_to_rand_sample
+
+
     # calculates cost between 2 vertexes
     def calc_cost(self, a, b):
-        #TODO: ori should give us
-
-    #by given delta, v and t the function return new point (x,y,theta)
-    def control_command(self, a, b):
         # TODO: ori should give us
+        return
 
-    # creates a new vertex in graph towards the goal
-    def bias_expanding(self): #TODO: chaim
-        nearest_vertex, nearest_vertex_index = self.find_vertex_nearest_to_sample(self.goal)
-        next_vertex = self.create_next_vertex_between_sample_and_nearest_vertex(nearest_vertex, self.goal,
-                                                                                    nearest_vertex_index)
-        result = self.connect_vertexes(nearest_vertex, nearest_vertex_index, next_vertex)
-        if self.calc_distance(self.goal, sample) < self.goal_radius:
-            self.add_vertex(self.goal, parent_index)
+
+    # by given delta, v and t the function returns new point (x,y,theta)
+    def control_command(self, vertex, delta, t, v):
+        # TODO: ori should give us
+        return
+
+
+    # checks if vertex is within track boundaries
+    def is_in_track(self, x, y):
+        # TODO: add later on, check if new vertex is within track boundaries
+        return True
+
+
+    # creates a new vertex in graph
+    def expand_graph(self):
+        # choose an existing vertex to expand from
+        if self.iteration % 10 == 0: # bias expanding TODO: ask ori of essence of bias here
+            parent_of_next_vertex = self.find_nearest_vertex_to_end(self.goal)
+        else: # random expanding
+            parent_of_next_vertex = choose_random_existing_vertex()
+        # keep getting a new vertex to expand to, and stop when it's within the track boundaries
+        first_iteration = True
+        while not is_in_track(x, y) or first_iteration == True:
+            delta, v, t = choose_random_parameters(parent_of_next_vertex)
+            x, y, theta = control_command(parent_of_next_vertex, delta, v, t)
+            first_iteration = False
+        # create the new vertex object and add it to the vertexes list
+        next_vertex = Vertex(x, y, theta, delta, v, parent_of_next_vertex.cost + t, self.vertexes.index(parent_of_next_vertex))
+        self.add_vertex(next_vertex)
+        # check if we got to the end vertex
+        if self.is_in_end_radius(next_vertex):
             self.success = True
-        return sample
-        return result, (next_vertex[0], next_vertex[1]), (nearest_vertex[0], nearest_vertex[1])
 
-    # creates a new vertex in graph towards a random sample
-    def random_expanding(self):#TODO: chaim
-        random_sample = self.create_random_sample()
-        nearest_vertex, nearest_vertex_index = self.find_vertex_nearest_to_sample(random_sample)
-        next_vertex = self.create_next_vertex_between_sample_and_nearest_vertex(nearest_vertex, random_sample,
-                                                                                    nearest_vertex_index)
-        # result is true id connecting vertexes work
-        result = self.connect_vertexes(nearest_vertex, nearest_vertex_index, next_vertex)
-        return result, random_sample, (next_vertex[0], next_vertex[1]), (nearest_vertex[0], nearest_vertex[1])
 
-    # draw the path in red
+    # draws the path in red
     def get_path_to_goal(self):
         if self.success:
             self.path = []
@@ -89,23 +146,32 @@ class Graph:
                 parent_index = vertex[2]
         return self.path
 
-    #loop for all existing vertexes and find the fastest path to new vertex from start point
-    def wiring(self, new_vertex):  #TODO: dana
 
-    #loop for all existing vertexes and for each existing vertex we will ask if the new vertex improves the path to existing vertex from start point
-    def rewiring(self, new_vertex): #TODO: dana
+    # loop for all existing vertexes and find the fastest path to new vertex from start point
+    def wiring(self, new_vertex):  #TODO: Dana the queen
+        return
+
+
+    # loop for all existing vertexes and for each existing vertex we will ask if the new vertex improves the path to existing vertex from start point
+    def rewiring(self, new_vertex): #TODO: Dana the queen
+        return
+
 
         '''
-        1. choose random vertex to be the parent
-        2. choose random delta, v, t
-        3. calculate new vertex - this is the control command that ori will give us
+        # Algorithm:
+        1. choose random vertex to be the parent.
+        2. choose random delta, v, t.
+        3. calculate new vertex (using the control command that ori will give us).
         4. wiring - loop for all existing vertexes and find the fastest path to new vertex from start point
             (at the end of this step we should have the new vertex and the path from start point to it in the tree)
-        5. rewire - loop for all existing vertexes and for each existing vertex we will ask if the new vertex improves the path to existing vertex from start point
+        5. rewire - loop for all existing vertexes and for each existing vertex we will ask if the new vertex improves
+            the path to existing vertex from start point
 
-        #ori will give us a control command that by given 2 vertexes it return the tine of getting from one to the other
+        # To be delivered by Ori:
+         1. control command that by given delta, v and t returns  a destination point (x, y, theta).
+         2. control command that by given 2 vertexes returns the time of getting from one to the other.
 
-        #for the future:
+        # Future notes:
         1. by given point to check if new vertex is within track boundaries.
         2. what if there are a few good paths to the end point
 
